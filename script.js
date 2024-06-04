836ee08b7c757b9190b58c9d75104fd4352747e4
JAMILE VITORIA ALVES DA SILVA <00001088304357sp@aluno.educacao.sp.gov.br>
2024-06-04 13:55:09 -0300

    trabalho incpmpleto

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert ("teste")
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fIMC =fPeso/(fAltura*fAltura)
+}
